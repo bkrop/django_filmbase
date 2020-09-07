2e82c1114809cf27546c5a381466575f5beb5ee3
Bartosz Kropiwnicki <kropiwnickib@gmail.com>
2020-09-07 21:48:02 +0200

    working on ajax comments

--- main/static/js/replies.js
+++ main/static/js/replies.js
@@ -1,14 +1,22 @@
 $(document).ready(function () {
     $(".showReplies").click(function () {
         let id = $(this).attr('id');
+        $(`#${id}.replies`).toggle();
         $.ajax({
             url: $(this).attr("data-href"),
             type: 'get',
 
             success: function (data) {
                 console.log(data);
-                console.log(id)
                 $(`#${id}.replies`).html(data);
+                let text = $(`#${id}.showReplies`).text()
+                if (text == 'Pokaż odpowiedzi') {
+                    text = 'Ukryj'
+                    $(`#${id}.showReplies`).text(text)
+                } else {
+                    text = 'Pokaż odpowiedzi'
+                    $(`#${id}.showReplies`).text(text)
+                }
             }
         })
     });
