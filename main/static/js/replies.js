03ba86670210410564dd9bed2efe91ce22bace2e
Bartosz Kropiwnicki <kropiwnickib@gmail.com>
2020-10-01 19:18:52 +0200

    comment likes

--- main/static/js/replies.js
+++ main/static/js/replies.js
@@ -1,26 +1,6 @@
 $(document).ready(function () {
     $(".showReplies").click(function () {
         let id = $(this).attr('id');
-        $.ajax({
-            url: $(this).attr("data-href"),
-            type: 'get',
-
-            success: function (data) {
-                console.log(data);
-
-                $(`#${id}.replies`).html(data);
-                let text = $(`#${id}.showReplies`).text();
-                console.log(text);
-                if (text == 'Pokaż odpowiedzi') {
-                    text = 'Ukryj';
-                    $(`#${id}.showReplies`).text(text);
-                    $(`#${id}.replies`).show();
-                } else {
-                    text = 'Pokaż odpowiedzi';
-                    $(`#${id}.showReplies`).text(text);
-                    $(`#${id}.replies`).hide();
-                }
-            }
-        })
-    });
-});
+        let div = $(`#${id}.replies`)
+        let text = $(`#${id}.showReplies`).text();
+        div.toggle();})})
